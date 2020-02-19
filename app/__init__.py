004d3507243f1d2f1cfc54a326a69f976242b4e8
Dominic Henry <dominichenrywork@hotmail.com>
2020-02-18 21:48:21 -0500

    Submission

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "9rj)(wBEGhbP_<Ka"
+app.config["MAIL_SERVER"] = "smtp.mailtrap.io"
+app.config["MAIL_PORT"] = "465"
+app.config["MAIL_USERNAME"] = "3c4cf6a715e870"
+app.config["MAIL_PASSWORD"] = "2519a1a4fe3a33"
+
+mail = Mail(app)
 from app import views
+
